ff118a962c705ec573231e23dc2251a0cf785300
Demonchannel <danik1808@rambler.ru>
2022-12-05 18:01:38 +0100

    Something

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './Pages/about-me/about-me.component';
+import { ExperienceComponent } from './Pages/experience/experience.component';
+import { GuestComponent } from './Pages/guest/guest.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
+import { RegistrComponent } from './Pages/registr/registr.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'guest', component: GuestComponent},
+  { path: 'experience', component: ExperienceComponent  },
+  { path: 'login', component: LoginComponent},
+  { path: 'reg', component: RegistrComponent},
+  { path: '**', component: NotFoundComponent},
+  {path: 'aboutMe', component: AboutMeComponent}
+  // { path: '**', redirectTo: '/' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { AboutMeComponent } from './Pages/about-me/about-me.component';
+import { ExperienceComponent } from './Pages/experience/experience.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
+import { GuestComponent } from './Pages/guest/guest.component';
+import { DataService } from './Pages/data.service';
+import { AuthService } from './Pages/services/auth.service';
+import {AngularFireModule} from '@angular/fire/compat'
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    LoginComponent,
+    NotFoundComponent,
+    GuestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
-  providers: [],
+  providers: [DataService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
